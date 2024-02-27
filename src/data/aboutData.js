1e424119c4936365a9840d0f4fca4511ae058609
SV-Developers <freelancesv20@gmail.com>
2024-02-27 11:03:47 +0530

    Delete src directory

--- src/data/aboutData.js
+++
@@ -1,8 +0,0 @@
-export const aboutData = {
-  title: "Who I am",
-  description1:
-    "We are SV-Developers a dynamic team with speciality in graphics design & web development based in India.",
-  description2:
-    "SV-Developers is a dynamic freelance team specializing in web development and data analytics, utilizing a diverse array of technologies to deliver tailored solutions. Our proficiency spans React.js, Node.js, Next.js, Express.js, MongoDB, Firebase, MySQL, Jira, WordPress, MS Excel, MS Office, MS Word, Power BI, Prompt Engineering, C/C++, Java, and Python. Additionally, we excel in the realm of ML/AI, actively engaging in automation and extensions. With a keen understanding of modern tech stacks and an emphasis on cutting-edge solutions, we bring versatility and innovation to every project. Our commitment to staying at the forefront of technology ensures that SV-Developers delivers high-quality, future-proof solutions that meet the evolving needs of our clients in the dynamic landscape of web development and data analytics.",
-  image: 2,
-};
--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'freelancesv20@gmail.com',
-    phone: '+918340534900',
-    address: 'Bengaluru, Karnataka, India',
-
-    sheetAPI: ''
-}
